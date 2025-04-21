2a652edf1b9d5d43ffe2aabc523164eeaadc55d3
LosOleksandr <losalexander2003@gmail.com>
2025-04-21 16:15:31 +0300

    feat: add metadata for root layout, add fonts

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,6 @@
 export default function Home() {
   return (
-    <div className="grid min-h-dvh place-items-center">
+    <div className="grid min-h-dvh place-items-center font-sans">
       <h1 className="text-3xl font-bold">Home</h1>
     </div>
   );
